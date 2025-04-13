0db32938f7f88d3fc362701c07ed2d4803bb3601
Madhumidha-S <madhumidha072005@gmail.com>
2025-04-13 23:18:05 +0530

    Database Related Implementation

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const db = require("../utils/database");
 
 let num = 10000000000000;
 
@@ -37,6 +38,12 @@ exports.generateURL = async (req, res, next) => {
     console.log(
       `Generated Short ID: ${shortID} for Value ${num} and ShortURL: ${shortenedURL}, LongURL: ${longURL}`
     );
+    const query = `
+      INSERT INTO urls (short_id, long_url, created_at)
+      VALUES ($1, $2, $3)
+    `;
+    await db.query(query, [shortID, longURL, new Date()]);
+
     res.status(200).json({
       longURL,
       shortenedURL,
@@ -46,5 +53,23 @@ exports.generateURL = async (req, res, next) => {
   } catch (error) {
     next(error);
     res.status(500).json({ error: "Failed to generate Shortened URL" });
+  }
+};
+
+exports.deleteURL = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ error: "ID Parameter is required" });
+    const result = await db.query(`SELECT * FROM urls WHERE id = $1`, [id]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "URL not found" });
+    }
+    await db.query(`DELETE FROM urls WHERE id = $1`, [id]);
+    res
+      .status(200)
+      .json({ message: "URL deleted successfully", deleted: result.rows[0] });
+  } catch (error) {
+    next(error);
+    res.status(500).json({ error: "Failed to delete URL" });
   }
 };
