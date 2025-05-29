f02411b5654c76e68cc3f0cd5b705fa1b63227c4
Madhumidha-S <madhumidha072005@gmail.com>
2025-05-29 22:11:28 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = require("./app");
-app.use(express.json());
+// app.use(express.json());
 const PORT = process.env.PORT || 3000;
-const routes = require("./routes");
-app.use("/", routes);
+// const routes = require("./routes");
+// app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
 });
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'URL Shortener API', 
-    version: '1.0.0'
-  });
-});
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'URL Shortener API',
+//     version: '1.0.0'
+//   });
+// });
